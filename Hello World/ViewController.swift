971c85b04e48d14f033cd685d0efb6c5a0944fa2
Zachary Pierucci <zacharypierucci@Zacharys-MacBook-Pro.local>
2018-08-24 21:54:39 -0500

    finished project

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        welcomeLabel.text = "Welcome!"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func helloPressed(_ sender: UIButton) {
+        welcomeLabel.text = "Hello World!"
+    }
+    @IBAction func clearPressed(_ sender: UIButton) {
+        welcomeLabel.text = ""
+    }
+    
 }
 
